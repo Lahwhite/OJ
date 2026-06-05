b5d92a77270ae99db45b4b7e3d83a0631e50fab8
pzt <lanadelrey08@users.noreply.github.com>
2026-06-05 15:37:50 +0800

    docs: add comments to repository and exception classes

--- Problems/src/main/java/com/oj/problem/repository/TagRepository.java
+++ Problems/src/main/java/com/oj/problem/repository/TagRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<TagEntity, Long> {
 
+    // 批量按名字查，创建/更新题目时用来同步标签
     List<TagEntity> findByNameIn(Collection<String> names);
 }
